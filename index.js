5c34b89d76abc46858395d7ae71482099786c600
sabberrahman <sabbersr1@gmail.com>
2025-03-13 14:29:13 +0600

    payload data limit 100kb to 10MB

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import cors from "cors"
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
 
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(cors())
-
+app.use(bodyParser.json({ limit: '10mb' })); 
 
 let storedData = null; 
 
